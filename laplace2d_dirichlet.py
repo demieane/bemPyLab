b7e63ff7024f07a09287ff2fa2eb6ec061bb1d1d
demieane <dimitranevlavi@gmail.com>
2020-09-15 00:43:39 +0300

    Update laplace2d_dirichlet.py

--- laplace2d_dirichlet.py
+++ laplace2d_dirichlet.py
@@ -11,11 +11,7 @@ def constantStrengthSource(x1, y1, x2, y2, xp, yp):
     # The induced velocity and velocity potential due to SL panel
     # At the centre of the element
     # curvilinear coordinate system
-    #uu = 0.0  #self-induced velocity in x-axis
-    #vv = -0.5 #self-induced velocity in y-axis
-    xm=0.5*(x1+x2);
-    ym=0.5*(y1+y2);
-
+    xm=0.5*(x1+x2); ym=0.5*(y1+y2)
     xi1 = 0
     xi2 = math.sqrt((x2-x1)**2+(y2-y1)**2)
 
@@ -32,22 +28,12 @@ def constantStrengthSource(x1, y1, x2, y2, xp, yp):
         th1 = math.atan2(yip,xip-xi1)
         th2 = math.atan2(yip,xip-xi2)
 
-        #uu = math.log(r1/r2)/2/math.pi
-        #vv = (th2-th1)/2/math.pi
-
         fi = (2*(xip-xi1)*math.log(r1) - 2*(xip-xi2)*math.log(r2) -2*(x2-x1) + 2*yip*(th2-th1))/4/math.pi
 
-    # global coordinate system
-
-    #f1 = uu*math.cos(th0)-vv*math.sin(th0) #u
-    #f2 = uu*math.sin(th0)+vv*math.cos(th0) #v
-    #f3 = fi; #potential
-    f = fi#[f1, f2, f3]
-    return f
+    return fi
 
 def constantStrengthDoublet(x1, y1, x2, y2, xp, yp):
-    xm=0.5*(x1+x2)
-    ym=0.5*(y1+y2)
+    xm=0.5*(x1+x2); ym=0.5*(y1+y2)
     xi1=0.0
     xi2=math.sqrt((x2-x1)**2+(y2-y1)**2)
     th0=math.atan2(y2-y1,x2-x1)
@@ -63,16 +49,14 @@ def constantStrengthDoublet(x1, y1, x2, y2, xp, yp):
     return fi
 
 def lineParametrization(xa, ya, xb, yb, Npanels):
-    # from A to B
+    # line segment from point A to point B
     t = np.linspace(0,1,Npanels)
-    #print(t)
     xline = xa + t*(xb-xa)
     yline = ya + t*(yb-ya)
     return xline, yline
 
 def collocationScheme(xi, yi):
     #normal and tangent unit vectors
-    #xtest = [1,2]
     sizeN= len(xi)-1
     xcolloc = np.zeros(sizeN);ycolloc = np.zeros(sizeN)
     sin_theta = np.zeros(sizeN);cos_theta = np.zeros(sizeN)
@@ -104,14 +88,14 @@ def collocationScheme(xi, yi):
 def show():
    return matplotlib.pyplot.show(block=True)
 
-#===========================MAIN CODE BELOW====================================#
+# MAIN CODE
 
-#==============CREATE MESH=====================================================#
+# CREATE MESH FOR SQUARE DOMAIN & BOYNDARY CONDITIONS
 plotMesh = 1
-plotSolution = 1
+plotAnalyticSolution = 0
 plotBC = 1
 
-Np = 20 #number of panels
+Np = 20 #number of nodes
 # square domain creation
 xa, ya = 0, 0
 xb, yb = math.pi, 0
@@ -122,23 +106,23 @@ xd, yd = 0, 1
 [x3, y3] =lineParametrization(xc, yc, xd, yd, Np) #y=1
 [x4, y4] =lineParametrization(xd, yd, xa, ya, Np) #x=0
 
+#nodes on the boundary with counter-clockwise numbering
+xi = np.concatenate((x1, x2[1:len(x2)], x3[1:len(x3)], x4[1:len(x4)]))
+yi = np.concatenate((y1, y2[1:len(y2)], y3[1:len(y3)], y4[1:len(y4)]))
+
+[xcolloc, ycolloc, nx, ny, tx, ty] = collocationScheme(xi, yi)
+xcolloc3 = xcolloc[2*(Np-1):3*(Np-1)]
 #Boundary conditions
 #   D---------------C
 #   -               -
 #   -               -
 #   A---------------B
-bc1 = np.zeros(Np) #[xa,ya] - [xb, yb]
-bc2 = np.zeros(Np) #[xb,yb] - [xc, yc]
-bc3 = np.sin(x3) #[xc,yc] - [xd, yd]
-bc4 = np.zeros(Np) #[xd,yd] - [xa, ya]
-
-#nodes on the boundary with counter-clockwise numbering
-xi = np.concatenate((x1, x2[1:len(x2)], x3[1:len(x3)], x4[1:len(x4)]))
-yi = np.concatenate((y1, y2[1:len(y2)], y3[1:len(y3)], y4[1:len(y4)]))
-bci = np.concatenate((bc1, bc2[1:len(bc1)], bc3[1:len(bc3)],bc4[1:len(bc4)]))
-xindex = np.linspace(0,1, len(bci))
-
-[xcolloc, ycolloc, nx, ny, tx, ty] = collocationScheme(xi, yi)
+u_bc1 = np.zeros(Np-1) #[xa,ya] - [xb, yb]
+u_bc2 = np.zeros(Np-1) #[xb,yb] - [xc, yc]
+u_bc3 = np.sin(xcolloc3) #[xc,yc] - [xd, yd]
+u_bc4 = np.zeros(Np-1) #[xd,yd] - [xa, ya]
+u_bc = np.concatenate((u_bc1, u_bc2, u_bc3, u_bc4))
+xindex = np.linspace(0, 1, len(u_bc))
 # plot the discretized domain
 if (plotMesh==1):
     plt.plot(x1, y1, x2, y2, x3, y3, x4, y4)
@@ -153,67 +137,66 @@ if (plotMesh==1):
     show()
 
 if (plotBC == 1):
-    plt.plot(xindex, bci, xindex, bci, 'bo')
+    plt.plot(xindex, u_bc, xindex, u_bc, 'bo')
     plt.title('Dirichlet boundary conditions')
     show()
 
-#analytic solution
-if (plotSolution==1):
+# ANALYTIC SOLUTION PREVIEW
+if (plotAnalyticSolution==1):
     xanal = np.linspace(xa, xb, 100)
     yanal = np.linspace(ya, yc, 100)
     xx, yy = np.meshgrid(xanal, yanal)
-
     uanal = np.sinh(yy)*np.sin(xx)/math.sinh(1)
     plt.contourf(xx,yy,uanal)
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.plot_surface(xx, yy, uanal, cmap=cm.coolwarm,
-                           linewidth=0, antialiased=False)
+    ax.plot_surface(xx, yy, uanal, cmap=cm.coolwarm,linewidth=0, antialiased=False)
     ax.set_xlabel('X axis')
     ax.set_ylabel('Y axis')
     ax.set_zlabel('Z axis')
     plt.show()
 
+
+# BOUNDARY ELEMENT METHOD (BEM) BOYNDARY INTEGRAL EQUATION IN MATRIX FORM
 sizeA = len(xcolloc)
-Abij = np.zeros((sizeA, sizeA))
-Bbij = np.zeros((sizeA, sizeA))
-udirichlet = np.zeros((sizeA,1))
+Abij = np.zeros((sizeA, sizeA)) #G
+Bbij = np.zeros((sizeA, sizeA)) #dGdn
+#udirichlet = np.zeros((sizeA,1))
 A = np.zeros((sizeA, sizeA))
 S = np.zeros((sizeA, sizeA))
 
 for ii in range(0, sizeA): #for each collocation point
-    for jj in range(0, sizeA): #we evaluate the effect of source distribution on the panels
-        # [u, v, fi]
+    for jj in range(0, sizeA): #we evaluate the effect each panel
+        # on each panel we have a piecewise-constat distribution of
+        # sources (G) and dipoles (dG/dn)
         xp, yp = xcolloc[ii], ycolloc[ii]
         x1, y1 = xi[jj], yi[jj]
         x2, y2 = xi[jj+1], yi[jj+1]
-        Abij[ii][jj] = constantStrengthSource(x1, y1, x2, y2, xp, yp) #w
-        Bbij[ii][jj] = constantStrengthDoublet(x1, y1, x2, y2, xp, yp)#dw/dn
+        Abij[ii][jj] = constantStrengthSource(x1, y1, x2, y2, xp, yp)
+        Bbij[ii][jj] = constantStrengthDoublet(x1, y1, x2, y2, xp, yp)
         deltaKronecker = 0
         if (ii == jj):
             deltaKronecker = 1
         A[ii][jj] = 0.5*deltaKronecker + Bbij[ii][jj]
         S[ii][jj] = Abij[ii][jj]
 
-    udirichlet[ii][0] = bci[ii]
+# SOLUTION OF THE LINEAR SYSTEM
+b = np.matmul(A, u_bc)
+dudn_bc = np.linalg.solve(S,b)
 
-b = np.matmul(A, udirichlet)
-ubem = np.linalg.solve(S,b)
-
-# comparison
+# COMPARISON WITH THE ANALYTIC SOLUTION
 xindex = np.linspace(0, 1, len(xcolloc))
-plt.plot(xindex, udirichlet)
-plt.plot(xindex, udirichlet, 'bo', label='dirichlet data')
-plt.plot(xindex, ubem, 'r*')
+plt.plot(xindex, u_bc)
+plt.plot(xindex, u_bc, 'bo', label='dirichlet data')
+plt.plot(xindex, dudn_bc, 'r*', label='neumann data')
 strLabel = 'Collocation points counter-clockwise indexing from (' + str(xa) + ',' + str(ya) +')'
 plt.xlabel(strLabel);
 plt.legend(bbox_to_anchor=(1.05, 1), loc='upper right', borderaxespad=0.)
 plt.grid(linestyle='-', linewidth=0.5)
 show()
 
-#comparison of the analytic solution and the numerical from bem
-#make sure the conditions match
+# DOMAIN SOLUTION u(x,y)
 xtest = np.linspace(xa+0.01, xb-0.01, 20)
 ytest = np.linspace(ya+0.01, yc-0.01, 20)
 xnum, ynum = np.meshgrid(xtest, ytest)
@@ -223,15 +206,13 @@ for kk in range(0, len(xtest)):
     for ll in range(0, len(ytest)):
         # for each point in the meshgrid
         utemporary = 0
-        xp = xtest[ll];
-        yp = ytest[kk];
-        #print(xp, yp)
+        xp = xtest[ll]; yp = ytest[kk];
         for jj in range(0, len(xcolloc)):
             x1, y1 = xi[jj], yi[jj]
             x2, y2 = xi[jj+1], yi[jj+1]
-            SL = constantStrengthSource(x1, y1, x2, y2, xp, yp) #w
-            DL = constantStrengthDoublet(x1, y1, x2, y2, xp, yp)#dw/dn
-            utemporary = utemporary + ubem[jj][0]*SL - udirichlet[jj][0]*DL
+            SL = constantStrengthSource(x1, y1, x2, y2, xp, yp) # (G)
+            DL = constantStrengthDoublet(x1, y1, x2, y2, xp, yp) #(dG/dn)
+            utemporary = utemporary + dudn_bc[jj]*SL - u_bc[jj]*DL
 
         unum[kk,ll]=utemporary
 
@@ -249,4 +230,3 @@ ax.set_xlabel('X axis')
 ax.set_ylabel('Y axis')
 ax.set_zlabel('Z axis')
 plt.show()
-# BEWARE THE PLOT IS TITLED??? WHAT THE ACTUAL FUCK
